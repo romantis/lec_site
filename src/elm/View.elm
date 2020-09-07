67e99cf4421563e1e72f5b35b46346bd79a9aeae
Roman Tyshchenko <udatos@gmail.com>
2020-09-07 18:48:01 +0300

    Remove COVID-19 message (#13)

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -18,16 +18,7 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "LEC"
     , body =
-        [ div [ class "ls-covid19"] 
-            [ p [] [ text "Due to COVID-19 pandemic, all live meetings are stopped."]
-            , p [] 
-                [ text "Check out our "
-                , a [ href "https://www.facebook.com/groups/82542151162/", target "_blank" ] [ text "facebook"]
-                , text " for information about online meetings."
-                ]
-            
-        ]
-        , Header.view model.route model.navItems
+        [ Header.view model.route model.navItems
         , page model
         , Footer.view  model.date
         ]
